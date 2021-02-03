d2c486d2e5d41c8b0872b4d4d972295189ab5389
Titanmito <chretien.clement@yahoo.fr>
2021-02-03 17:05:11 +0100

    game motor WIP

---
+++ Assets/Scripts/Note.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Note : MonoBehaviour
+{
+
+    private Rigidbody2D rigidbody;
+    public float speed;
+
+    void Awake(){
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody.velocity = new Vector2(0, -speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/RandomNotesGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomNotesGenerator : MonoBehaviour
+{
+
+    public GameObject notesPrefab;
+    public float reloadTime = 0.3f;
+    public float reloadProgress = 0f;
+    private float randomX;
+    public System.Random r = new System.Random();
+    private float[] xPosition;
+    private Color[] notesColor;
+    private GameObject newObject;
+    private int rand;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        xPosition = new float[8] {-3.5f, -2.5f, -1.5f, -0.5f, 0.5f, 1.5f, 2.5f, 3.5f};
+        notesColor = new Color[8] {Color.black, Color.magenta, Color.red, Color.yellow, Color.green, Color.cyan, Color.blue, Color.white};
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        reloadProgress += Time.deltaTime;
+        if(reloadProgress >= reloadTime){
+            rand = r.Next(8);
+            randomX = xPosition[rand];
+            newObject = Instantiate<GameObject>(notesPrefab, new Vector2(randomX, 6), Quaternion.identity);
+            newObject.GetComponent<SpriteRenderer>().color = notesColor[rand];
+            reloadProgress = 0f;
+        }
+    }
+}
